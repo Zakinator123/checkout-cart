fd367f0403721d7d33e6057dcab33a7fbaeadf8f
Zakey Faieq <zakeyf@gmail.com>
2022-12-24 21:27:22 -0500

    Add action selector component

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,9 +1,16 @@
 import {initializeBlock} from '@airtable/blocks/ui';
 import React from 'react';
+import ActionSelector from "./ActionSelector";
 
 function HelloWorldTypescriptApp() {
     // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+    return (
+        <div>
+            Hello Test world 🚀
+            
+            <ActionSelector/>
+        </div>
+    );
 }
 
 initializeBlock(() => <HelloWorldTypescriptApp />);
