2abe6d7b4b0ae87e56d4f8f2e107ee12faf46f03
Zakey Faieq <zakeyf@gmail.com>
2023-03-05 11:55:51 -0500

    Add pre-commit hook

---
+++ scripts/pre-commit-hook.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "\n"
+echo "***** Running Pre-Commit Build ******"
+
+npm run pre-commit
+status=$?
+
+echo "\n"
+
+if [ $status -eq 0 ]; then
+  echo "***** Build Successful. Staging new coverage badge and committing the following changes: ******"
+  git add assets/coverage-badge.svg
+  git --no-pager diff --staged --name-only
+else
+  echo "***** Build failled, aborting commit. *****"
+fi
+
+echo "\n"
+
+exit $status
